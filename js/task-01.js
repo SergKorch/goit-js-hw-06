3265ff88127f52b1fde85ba3b62d35593e17ae75
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-01-30 12:15:29 +0200

    HW1

--- js/task-01.js
+++ js/task-01.js
@@ -1,32 +1,11 @@
 const numberLi = document.querySelector("#categories");
-const numberLib = document.querySelectorAll("ul>li");
-
-console.log("Category:", numberLi.textContent); //save
-
-const categories = document.querySelectorAll("li.item");
+const categories = numberLi.querySelectorAll("li.item");
 console.log("Number of categories:", categories.length);
-
-const elements = document.querySelectorAll("li.item>ul");
-elements.forEach((dish) => {
-    console.log("Elements11:", dish.firstChild.length);
-  
-  });
-
-const matches = document.querySelectorAll("li.item>h2");
-matches.forEach((dish) => {
-  console.log("Category:", dish.textContent);
-  console.log("Elements:", dish.firstChild.length);
-
+categories.forEach((categEl) => {
+  console.log("Category:", categEl.querySelector("h2").textContent);
+  console.log("Elements:", categEl.querySelectorAll("li").length);
 });
 
+// magicBtn.addEventListener('click', ()=>{
 
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
+// });
