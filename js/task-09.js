44b22d054eaee64fddbb1d3b3652b2fe12ac98dd
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-03 17:13:04 +0200

    HW09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const form = document.querySelector("body");
+
+const btn = document.querySelector(".change-color");
+const span = document.querySelector("span");
+
+btn.addEventListener("click", getHexColor);
+function getHexColor () {
+form.style.backgroundColor = getRandomHexColor();
+span.textContent=form.style.backgroundColor
+console.log(getRandomHexColor());
 }
