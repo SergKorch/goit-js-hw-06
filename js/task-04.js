2b2b09d77d3bb3836407a56b8136d2291feb6f86
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-06 00:08:16 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,29 @@
-const btns = document.querySelectorAll(".counter_btn");
-let currentValue = 0;
-btns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    const action = this.dataset.action;
-    const span = this.parentElement.querySelector("#value");
-   currentValue = +span.textContent;
-    let newValue;
-    if (action === "increment") {
-      newValue = currentValue + 1;
-    } else {
-      newValue = currentValue - 1;
-    }
-    span.textContent = newValue;
-  });
-});
+const btnDec = document.querySelector('[data-action="decrement"]');
+const btnInc = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value');
+let counterValue = 0;
+const btnClickDec = event => {
+  value.textContent = counterValue += 1;
+};
+const btnClickInc = event => {
+  value.textContent = counterValue -= 1;
+};
+btnDec.addEventListener('click', btnClickDec);
+btnInc.addEventListener('click', btnClickInc);
+
+// const btns = document.querySelectorAll(".counter_btn");
+// let currentValue = 0;
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", function () {
+//     const action = this.dataset.action;
+//     const span = this.parentElement.querySelector("#value");
+//    currentValue = +span.textContent;
+//     let newValue;
+//     if (action === "increment") {
+//       newValue = currentValue + 1;
+//     } else {
+//       newValue = currentValue - 1;
+//     }
+//     span.textContent = newValue;
+//   });
+// });
