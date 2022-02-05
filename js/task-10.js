c7d3ce0a8e04c9fa2ccec5d5d7fd5767fffddcf1
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-05 17:04:37 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,16 +5,16 @@ create.addEventListener("click", getAmount);
 destroy.addEventListener("click", destroyBoxes);
 
 function getAmount() {
-  var amount = +document.querySelector("#controls input").value;
+  let amount = +document.querySelector("#controls input").value;
   createBoxes(amount);
 }
 
 function createBoxes(amount) {
-  var basicSize = 30;
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < amount; i++) {
-    var size = basicSize + i * 10;
-    var div = document.createElement("div");
+  const basicSize = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = basicSize + i * 10;
+    const div = document.createElement("div");
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};`;
     fragment.appendChild(div);
   }
