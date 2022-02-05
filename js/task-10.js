387a5b23135e0d4d4940702221d1267e7b566f68
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-05 17:02:57 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,41 +1,32 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// const divSection = document.querySelector('#boxes');
-// const divElAll=[];
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+const boxes = document.getElementById("boxes");
+create.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
 
-// function addDiv ()  {
-// const divEl=document.createElement("div");
-// console.log(divEl);
-// divElAll.push(divEl);
-// }
+function getAmount() {
+  var amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
 
-// const button = document.querySelector(".btn");
-
-// console.log(divSection);
-// divSection.append(...divElAll)
-
-// const  createBoxes = amount => {
-
-// };
-const btns = document.querySelectorAll("button");
-console.log(...btns);
-// let currentValue = 0;
-btns.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      const action = this.dataset.action;
-  //     const span = this.parentElement.querySelector("#value");
-  //    currentValue = +span.textContent;
-  //     let newValue;
-  if (action === "create") {
-    const divEl = document.createElement("div");
-    console.log(divEl);
-    divElAll.push(divEl);
-  } else if ((action === "destroy")) {
-        divElAll.remove();
+function createBoxes(amount) {
+  var basicSize = 30;
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < amount; i++) {
+    var size = basicSize + i * 10;
+    var div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};`;
+    fragment.appendChild(div);
   }
-  //     span.textContent = newValue;
-});
-// });
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
