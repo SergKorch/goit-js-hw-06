f197d3e5d91ec2bfd2b2aa8bba84bc59570f0f66
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-06 09:33:44 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,6 @@
 const inp = document.getElementById("font-size-control");
-
 const span = document.querySelector("#text");
-
+span.style.fontSize = inp.value + "px";
 inp.addEventListener("input", inpMove);
 function inpMove(event) {
   span.style.fontSize = event.currentTarget.value + "px";
